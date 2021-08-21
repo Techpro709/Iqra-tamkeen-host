edf0443f6ca952903f4b088f8c6e4135a6997417
TechPro709 <61668117+Techpro709@users.noreply.github.com>
2021-08-20 17:25:16 -0700

    bruh

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var ar1 = 'don\x27t see file';
 var en1 = "don\x27t see file";
-var file= '';
+var file = '';
 console.log("Script.js Loaded");
 
 window.onbeforeunload = function () {
@@ -40,9 +40,9 @@ function includeHTML() {
 window.addEventListener("load", myInit, true); function myInit() {
   includeHTML();
   w3IncludeHTML();
-  
-// readTextFile("https://raw.githubusercontent.com/Techpro709/Iqra-tamkeen-host/main/text/Arabic-Lorem.txt");
-// readTextFile("https://raw.githubusercontent.com/Techpro709/Iqra-tamkeen-host/main/text/English-Lorem.txt");
+
+  // readTextFile("https://raw.githubusercontent.com/Techpro709/Iqra-tamkeen-host/main/text/Arabic-Lorem.txt");
+  // readTextFile("https://raw.githubusercontent.com/Techpro709/Iqra-tamkeen-host/main/text/English-Lorem.txt");
 };
 // function readTextFile(file) {
 //   var rawFile = new XMLHttpRequest();
@@ -57,7 +57,7 @@ window.addEventListener("load", myInit, true); function myInit() {
 //         }
 //       }
 //     } else if (file == 'https://raw.githubusercontent.com/Techpro709/Iqra-tamkeen-host/main/text/English-Lorem.txt')
-      
+
 //     if (rawFile.readyState === 4) {
 //       if (rawFile.status === 200 || rawFile.status == 0) {
 //         en1 = rawFile.responseText;
@@ -74,45 +74,82 @@ window.addEventListener("load", myInit, true); function myInit() {
 // readTextFile("https://raw.githubusercontent.com/Techpro709/Iqra-tamkeen-host/main/text/English-Lorem.txt");
 
 // read text from URL location
-getText(en1)
+// getText(en1)
 
-function getText(en1){
-  var request = new XMLHttpRequest();
-  request.open('GET', 'https://raw.githubusercontent.com/Techpro709/Iqra-tamkeen-host/main/text/English-Lorem.txt', true);
-  request.send(null);
-  request.onreadystatechange = function () {
-      if (request.readyState === 4 && request.status === 200) {
-          var type = request.getResponseHeader('Content-Type');
-          if (type.indexOf("text") !== 1) {
-            en1 = request.responseText;
-            window.en1 = request.responseText;
-              console.log(en1);
-              return en1;
-             
-          }
-      }
-  }
-};
+// function getText(en1){
+//   var request = new XMLHttpRequest();
+//   request.open('GET', 'https://raw.githubusercontent.com/Techpro709/Iqra-tamkeen-host/main/text/English-Lorem.txt', true);
+//   request.send(null);
+//   request.onreadystatechange = function () {
+//       if (request.readyState === 4 && request.status === 200) {
+//           var type = request.getResponseHeader('Content-Type');
+//           if (type.indexOf("text") !== 1) {
+//             en1 = request.responseText;
+//             window.en1 = request.responseText;
+//             console.log(en1);
+//             return en1;
 
+//           }
+//       }else{
+//         return false;
+//       }
+//       alert(en1);
+//   }
+// };
+fetch('https://raw.githubusercontent.com/Techpro709/Iqra-tamkeen-host/main/text/English-Lorem.txt')
+  .then(response => response.text())
+  .then((data) => {
+    console.log(data)
+    en1 = data;
+    fetch('https://raw.githubusercontent.com/Techpro709/Iqra-tamkeen-host/main/text/Arabic-Lorem.txt')
+      .then(response => response.text())
+      .then((data) => {
+        console.log(data)
+        ar1 = data;
+        fetchDun()
+      })
+  })
 
-alert(en1);
 var dictionary = {
   'button-1': {
     'en': 'Switch to Arabic<br>قم بالتبديل إلى اللغة العربية',
     'ar': 'Switch to English<br><span lang="ar">قم بالتبديل إلى اللغة الإنجليزية</span>',
-  },
-  'lorem': {
-    'en': en1,
-    'ar': ar1,
   }
 };
+
+function fetchDun() {
+  dictionary = {
+    'button-1': {
+      'en': 'Switch to Arabic<br>قم بالتبديل إلى اللغة العربية',
+      'ar': 'Switch to English<br><span lang="ar">قم بالتبديل إلى اللغة الإنجليزية</span>',
+    },
+    'lorem': {
+      'en': en1,
+      'ar': ar1,
+    }
+  };
+  translate();
+}
+function lang() {
+
+}
 var langs = ['en', 'ar'];
 var current_lang_index = 0;
 var current_lang = langs[current_lang_index];
 
 window.change_lang = function () {
-  current_lang_index = ++current_lang_index % 2;
-  current_lang = langs[current_lang_index];
+  x = document.getElementById("en").selected;
+  y = document.getElementById("ar").selected;
+  if (x == true) {
+    current_lang_index = 1;
+    current_lang = langs[current_lang_index];
+  }else if(y == true) {
+    current_lang_index = 1;
+    current_lang = langs[current_lang_index];
+  }
+
+  // current_lang_index = ++current_lang_index % 2;
+  // current_lang = langs[current_lang_index];
   translate();
 }
 
@@ -123,4 +160,7 @@ function translate() {
   });
 }
 
-translate();
+
+$(function () {
+  $('.selectpicker').selectpicker();
+});
